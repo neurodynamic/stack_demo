e538ebcdc90f037d637c5d3bc0d24cde31b895f8
Neurodynamic <neurodynamicdev@gmail.com>
2016-09-28 22:57:36 -0400

    get push working

--- src/Components/View.elm
+++ src/Components/View.elm
@@ -2,15 +2,16 @@ module Components.View exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Components.Model exposing (Model)
-import Components.Msg exposing (Msg)
+import Components.Msg exposing (..)
 import Components.Layout.Base exposing (layout)
 
 
 view : Model -> Html Msg
 view model =
     layout
-        [ Html.form [ class "push-form" ] [ text "push(", input [ class "push-input" ] [], text ")", button [] [ text "call" ] ]
+        [ Html.form [ class "push-form", onSubmit Push ] [ text "push(", input [ class "push-input", onInput NewInputValue, value model.inputValue ] [], text ")", button [] [ text "call" ] ]
         , div [ class "items" ] (List.map stringToBox model.items)
         ]
 
